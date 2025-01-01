cad28904a7ba20f6591dcacf85bfa1c64b8dfc46
Victor Schweitz <victorschweitz@hotmail.com>
2025-01-01 14:26:40 +0100

    Responsive navbar done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-alert('Hey!')
+const mobileMenuToggleBtn = document.querySelector('#mobile_menu_toggle_btn i')
+const mainNav = document.querySelector('#main_nav')
+
+
+mobileMenuToggleBtn.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu()
+{
+    mainNav.classList.toggle('active')
+
+    updateMobileMenuIcon()
+}
+
+function updateMobileMenuIcon()
+{
+    mobileMenuToggleBtn.classList.contains('fa-bars')
+        ? (mobileMenuToggleBtn.classList.remove('fa-bars'), mobileMenuToggleBtn.classList.add('fa-times'))
+        : (mobileMenuToggleBtn.classList.add('fa-bars'), mobileMenuToggleBtn.classList.remove('fa-times'));
+
+
+}
